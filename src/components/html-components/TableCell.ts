6742924d3f09c3d7741e7f1223df28f8d2fa73ee
Marcos Giordano <marcos.giordano@naranjax.com>
2023-05-28 12:40:41 +0200

    Bugfix: td and th are no containers

--- src/components/html-components/TableCell.ts
+++ src/components/html-components/TableCell.ts
@@ -1,15 +1,18 @@
+import ComponentChangeObserverInterface from '../common/interfaces/component-change-observer.interface';
+
 import TableCellBuilder from '../common/models/TableCellBuilder';
 import RawHTMLConponent from './RawHTMLComponent';
-import RawContainer from './RawContainer';
 
 import CssStyleSheet from '../css-stylesheet/css-stylesheet';
-import constants from '../common/constants/constants';
-import defineElementName from '../common/functions/define-element-name';
 
+import constants from '../common/constants/constants';
+
+import defineElementName from '../common/functions/define-element-name';
 import { AddComponentEnum } from '../common/enums/add-component.enum';
 
-export default class TableCell extends RawContainer {
+export default class TableCell extends RawHTMLConponent implements ComponentChangeObserverInterface {
     private static iterator = 0;
+
     constructor() {
         const name = defineElementName(`table_cell${TableCell.iterator++}`, RawHTMLConponent.instances);
 
@@ -31,12 +34,40 @@ export default class TableCell extends RawContainer {
             .build();
 
         super(element);
+
+        this.openElementConfigs = this.openElementConfigs.bind(this);
+        this.dragLeave = this.dragLeave.bind(this);
+
+        this._domElement.addEventListener('dragstart', this.dragStartWithTargetId);
+        this._domElement.addEventListener('dragleave', this.dragLeave);
+
+        this._domElement.addEventListener('click', this.openElementConfigs);
     }
 
-    protected addChildConfigs() {
+    get domElement() {
+        return this._domElement;
+    }
+
+    protected openElementConfigs(event) {
+        event.stopPropagation();
+        this.selectorValue();
+
+
+        this.buildElementConfigs();
+    }
+
+    public componentSelected(component) {
+        if(component === this._domElement['name']) {
+            this.buildElementConfigs();
+        }
+    }
+
+    private buildElementConfigs() {
         this.insertComponentAfter(
             AddComponentEnum.addInnerTextChangeComponent,
             AddComponentEnum.addClassNameDefinitionComponent
         );
+
+        this.buildElements();
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -11,30 +11,18 @@ createNewInstancePublisher.attach(appContainer);
 
 CssStyleSheet.init();
 
-// TODO: el metodo init deberia buscar el css del localstorage, deberiamos hacer lo mismo con el HTML
-// Deberiamos ver como almacenar y deshacer cambios....
-
-// TODO: acomodar estructuras con prettier y tslinter. Saltar linea cuando muy larga.
-
-// TODO: hay que meterle drag enter al app-container-fixed para que le saque el background color al app-container...
-// o ver que pasa con el drag leave
-
-// TODO: insertar rules para el media query : https://davidwalsh.name/add-rules-stylesheets
-// https://developer.mozilla.org/en-US/docs/Web/CSS/At-rule
-
 // TODO: probar exportar css y html y ver como queda la pagina en la realidad */
 // TODO: hacer analisis de que deberiamos exportar realmente del html */
 
-// TODO: aqui falta la accion de duplicar un componente, es decir, de un componente existente, duplicar la config...
+// TODO: falta la accion de duplicar un componente, es decir, de un componente existente, duplicar la config...
 // si, es necesario, porque es mucho mas facil que asignarle la clase de otro componente...
 // ver como duplicamos las clases... si le asignamos las clases del otro componente???, como hacemos con su clase???
 // si duplicamos, no creamos clase propia, solo asignamos las clases del otro componente...
 
-// Agregar el elemento img y a su vez, agregar imagenes al background...
+// Agregar imagenes al background...
 
 // TODO: falta todo lo que es position... Absolute, relative, etc... No recuerdo bien como es eso...
 // incluso, el z index, que podria estar en el mismo componente.
-
 
 // TODO: ver forma de remover el borde original del input. No es la misma propiedad que el border...
 // TODO: el borderbox del input viene como advanced... Ver si tenemos que hacerle algo
@@ -42,18 +30,18 @@ CssStyleSheet.init();
 // TODO: cuando creo un nombre futuro, y justo creo un elemento con ese nombre futuro, tira error...
 // por eso yo le pasaba el iterator... ver como solucionar...
 
-// TODO: con el id habria que reemplazar todos los caracteres especiales por guiones o algo asi...
+// TODO: con el id habria que reemplazar todos los caracteres especiales por guiones o algo asi...???
 
 // TODO: falta propiedad scrollable en div
 
-// TODO: cualquier elemento debe poder modificar el display settings
+// TODO: cualquier elemento debe poder modificar el display settings, solo que algunos no tendran el flex o grid
 
-// TODO: falta la propiedad background-image en el background-color
+// TODO: en el Select Item no se encuentra el app-container.
+// Es que para que esto funcione, el app-container deberia ser creado como un new Container();
+// De hecho, cuando creo un solo componente, no lo puedo seleccionar por el selector...
+// Ver si en vez de usar change, usamos 'select'
 
-// eslint-disable-next-line max-len
-// TODO: en el Select Item no se encuentra el app-container. Es que para que esto funcione, el app-container deberia ser creado como un new Container();
-
-// TODO: falta un boton de delete initial properties, asi sacamos los bordes y eso a un div, por ejemplo...
+// TODO: falta un boton de remove initial properties, asi sacamos los bordes y eso a un div, por ejemplo...
 
 /*
 TODO: faltan los siguientes componentes:
@@ -91,15 +79,30 @@ TODO: faltan los siguientes componentes:
 
     - hyperlink o anchor element: debemos agregar el hiperlink y algunas otras cosas
 
-    - textarea: debemos poder modificar el relleno, pero en vez de con un input, con un textarea
-    dar estilo tambien para que no sea estirable en el selector antes de hacer el drag and drop
+    - textarea: debemos poder modificar el relleno, pero en vez de con un input,
     tambien debe poder ser seleccionable si la queres estirable o no en el dom
 */
 
-// TODO: falta posicion absoluta o relativa, z-index
-
-// TODO: el color tambien podria ser en degrade, pero podemos dejar que eso lo hagan con la rawclass
+// TODO: el background-color tambien podria ser en degrade, pero podemos dejar que eso lo hagan con la rawclass
 
 // TODO: si yo a una clase le agrego, por ejemplo .table0>td,
 // nunca me trae esa clase como parte de la table0 en el Raw Class Editor
-// ni en el Classes Management Selector
+// ni en el Classes Management Selector, pero esta en el CssStyleSheet
+
+// TODO: cuando voy pasando por los contenedores, no le saca el hover al padre
+
+// TODO: cuando selecciono un contenedor, me muestra que le ppuedo agregar el hover que tiene a si mismo...
+// y cuando agrega la clase al classList del comp HTML, la agrega como contenedor0:hover
+
+
+
+
+
+// Para una V2 (por la complejidad):
+
+// TODO: el metodo init deberia buscar el css del localstorage, deberiamos hacer lo mismo con el HTML
+// Deberiamos ver como almacenar y deshacer cambios....
+
+// TODO: insertar rules para el media query : https://davidwalsh.name/add-rules-stylesheets
+// https://developer.mozilla.org/en-US/docs/Web/CSS/At-rule
+
